a58d37a69f1116fccb5932366870f4f326b5b2f4
jonah <kayiwa.jonah@student.moringaschool.com>
2024-02-27 17:41:04 +0300

    done

---
+++ lib/app.py
@@ -0,0 +1,21 @@
+#lib/app.py
+print("hello world!")
+print("hello sun!")
+print("hello sky!")
+print("hello stars!")
+print("hello mango!")
+
+##trying the end function
+print("hello world!", end=", ")
+print("hello sun!", end=", ")
+print("hello sky!", end=", ")
+print("hello stars!", end=",\n ")
+print("hello mango!", end=",\n ")
+
+##trying out functions
+price_1 = 1500
+price_2 = 3400
+price_3 = 2000
+
+print(f"the price of this item is {price_1}")
+print(f"the price of this item is ${price_3}")
